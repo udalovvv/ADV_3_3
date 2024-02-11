10036fe924e8261ab513ffcefe422c2a65e1b663
udalovvv <udalov.vitalii@gmail.com>
2024-02-11 16:01:58 +0500

    Add README.md

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,10 @@
 INSERT INTO users (`age`, `email`, `first_name`, `last_name`, `password`)
-VALUES ('1', 'admin@mail.ru', 'admin', 'admin', '$2a$12$cm066YTzIjDk0C361x6u1eKYiBsX0e/3abc5UCm.FvD5xdq4aSwLG');
--- password = 1
+VALUES ('1', 'admin@mail.ru', 'admin', 'admin', '$2a$12$iCZIGBvFU6Yud/n15FBFzu95xo4413RCEr2ou4FVdAxiv1WGmnpXC');
+-- password = admin
 
 INSERT INTO users (`age`, `email`, `first_name`, `last_name`, `password`)
-VALUES ('2', 'user@mail.ru', 'user', 'user', '$2a$12$cm066YTzIjDk0C361x6u1eKYiBsX0e/3abc5UCm.FvD5xdq4aSwLG');
--- password = 1
-
-INSERT INTO users (`age`, `email`, `first_name`, `last_name`, `password`)
-VALUES ('3', 'ua@mail.ru', 'ua', 'ua', '$2a$12$cm066YTzIjDk0C361x6u1eKYiBsX0e/3abc5UCm.FvD5xdq4aSwLG');
--- password = 1
+VALUES ('2', 'user@mail.ru', 'user', 'user', '$2a$12$1iGRrj7Ho1wyTRvBb73p1.peJUtfJRJOQ5nMLOTjZsIhPMNBuio9C');
+-- password = user
 
 INSERT INTO roles (`role`)
 VALUES ('ADMIN');
@@ -21,9 +17,3 @@ VALUES (1, 1);
 
 INSERT INTO users_roles (`user_id`, `role_id`)
 VALUES (2, 2);
-
-INSERT INTO users_roles (`user_id`, `role_id`)
-VALUES (3, 1);
-
-INSERT INTO users_roles (`user_id`, `role_id`)
-VALUES (3, 2);
